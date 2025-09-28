9d9df6c25b6405f3b48a427667d32cd22ee6df48
Illia <77698519+illia-fz@users.noreply.github.com>
2025-09-27 23:48:59 -0700

    Create random_quote.py

---
+++ random_quote.py
@@ -0,0 +1,8 @@
+import random
+QUOTES = [
+    'The only limit to our realization of tomorrow is our doubts of today. – Franklin D. Roosevelt',
+    'In the middle of every difficulty lies opportunity. – Albert Einstein',
+    'What you get by achieving your goals is not as important as what you become by achieving your goals. – Zig Ziglar',
+]
+if __name__ == '__main__':
+    print(random.choice(QUOTES))
